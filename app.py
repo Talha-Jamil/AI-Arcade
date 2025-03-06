ec3269febe29b1e569dd5b290ea8faecbab24cca
luqmanazhar5339 <luqmanazhar5339@gmail.com>
2025-03-06 21:05:31 +0500

    Added dino.html, improved frontend layout, and updated app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/dino')
+def dino():
+    return render_template('dino.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
